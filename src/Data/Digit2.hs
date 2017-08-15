c694f3ba20b11643878e6f73685ce1d50b312639
Tony Morris <tmorris@tmorris.net>
2017-08-15 16:33:22 +1000

    classy

--- src/Data/Digit2.hs
+++ src/Data/Digit2.hs
@@ -26,6 +26,8 @@ module Data.Digit2(
 ) where
 
 import Control.Lens
+import Text.Parser.Char
+import Text.Parser.Combinators((<?>), choice)
 
 type DecimalNoZero d =
   (D1 d, D2 d, D3 d, D4 d, D5 d, D6 d, D7 d, D8 d, D9 d)
@@ -59,6 +61,12 @@ instance D0 () where
   d0 =
     id
 
+parse0 ::
+  (D0 d, CharParsing p) =>
+  p d
+parse0 =
+  x0 <$ char '0' <?> "0"
+
 class D1 d where
   d1 ::
     Prism'
@@ -73,6 +81,12 @@ instance D1 () where
   d1 =
     id
     
+parse1 ::
+  (D1 d, CharParsing p) =>
+  p d
+parse1 =
+  x1 <$ char '1' <?> "1"
+
 class D2 d where
   d2 ::
     Prism'
@@ -87,6 +101,12 @@ instance D2 () where
   d2 =
     id
     
+parse2 ::
+  (D2 d, CharParsing p) =>
+  p d
+parse2 =
+  x2 <$ char '2' <?> "2"
+
 class D3 d where
   d3 ::
     Prism'
@@ -101,6 +121,12 @@ instance D3 () where
   d3 =
     id
     
+parse3 ::
+  (D3 d, CharParsing p) =>
+  p d
+parse3 =
+  x3 <$ char '3' <?> "3"
+
 class D4 d where
   d4 ::
     Prism'
@@ -115,6 +141,12 @@ instance D4 () where
   d4 =
     id
     
+parse4 ::
+  (D4 d, CharParsing p) =>
+  p d
+parse4 =
+  x4 <$ char '4' <?> "4"
+
 class D5 d where
   d5 ::
     Prism'
@@ -129,6 +161,12 @@ instance D5 () where
   d5 =
     id
     
+parse5 ::
+  (D5 d, CharParsing p) =>
+  p d
+parse5 =
+  x5 <$ char '5' <?> "5"
+
 class D6 d where
   d6 ::
     Prism'
@@ -143,6 +181,12 @@ instance D6 () where
   d6 =
     id
     
+parse6 ::
+  (D6 d, CharParsing p) =>
+  p d
+parse6 =
+  x6 <$ char '6' <?> "6"
+
 class D7 d where
   d7 ::
     Prism'
@@ -157,6 +201,12 @@ instance D7 () where
   d7 =
     id
     
+parse7 ::
+  (D7 d, CharParsing p) =>
+  p d
+parse7 =
+  x7 <$ char '7' <?> "7"
+
 class D8 d where
   d8 ::
     Prism'
@@ -171,6 +221,12 @@ instance D8 () where
   d8 =
     id
     
+parse8 ::
+  (D8 d, CharParsing p) =>
+  p d
+parse8 =
+  x8 <$ char '8' <?> "8"
+
 class D9 d where
   d9 ::
     Prism'
@@ -185,6 +241,12 @@ instance D9 () where
   d9 =
     id
     
+parse9 ::
+  (D9 d, CharParsing p) =>
+  p d
+parse9 =
+  x9 <$ char '9' <?> "9"
+
 class DA d where
   dA ::
     Prism'
@@ -199,6 +261,12 @@ instance DA () where
   dA =
     id
     
+parseA ::
+  (DA d, CharParsing p) =>
+  p d
+parseA =
+  xA <$ char 'A' <?> "A"
+
 class DB d where
   dB ::
     Prism'
@@ -213,6 +281,12 @@ instance DB () where
   dB =
     id
     
+parseB ::
+  (DB d, CharParsing p) =>
+  p d
+parseB =
+  xB <$ char 'B' <?> "B"
+
 class DC d where
   dC ::
     Prism'
@@ -227,6 +301,12 @@ instance DC () where
   dC =
     id
     
+parseC ::
+  (DC d, CharParsing p) =>
+  p d
+parseC =
+  xC <$ char 'C' <?> "C"
+
 class DD d where
   dD ::
     Prism'
@@ -241,6 +321,12 @@ instance DD () where
   dD =
     id
     
+parseD ::
+  (DD d, CharParsing p) =>
+  p d
+parseD =
+  xD <$ char 'D' <?> "D"
+
 class DE d where
   dE ::
     Prism'
@@ -255,6 +341,12 @@ instance DE () where
   dE =
     id
     
+parseE ::
+  (DE d, CharParsing p) =>
+  p d
+parseE =
+  xE <$ char 'E' <?> "E"
+
 class DF d where
   dF ::
     Prism'
@@ -268,4 +360,30 @@ class DF d where
 instance DF () where
   dF =
     id
+    
+parseF ::
+  (DF d, CharParsing p) =>
+  p d
+parseF =
+  xF <$ char 'F' <?> "F"
+
+----
+
+parseDecimal ::
+  (Decimal d, CharParsing p) =>
+  p d
+parseDecimal =
+  choice
+    [
+      parse0
+    , parse1
+    , parse2
+    , parse3
+    , parse4
+    , parse5
+    , parse6
+    , parse7
+    , parse8
+    , parse9
+    ] <?> "decimal"
     
