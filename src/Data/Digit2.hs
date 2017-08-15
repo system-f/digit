55bda5a5c3aaffa9576af4c706a985718fca9b6a
Tony Morris <tmorris@tmorris.net>
2017-08-15 16:22:54 +1000

    classy

---
+++ src/Data/Digit2.hs
@@ -0,0 +1,271 @@
+{-# LANGUAGE ConstraintKinds #-}
+
+module Data.Digit2(
+  DecimalNoZero
+, Decimal
+, OctalNoZero
+, Octal
+, HexadecimalNoZero
+, Hexadecimal
+, D0(..)
+, D1(..)
+, D2(..)
+, D3(..)
+, D4(..)
+, D5(..)
+, D6(..)
+, D7(..)
+, D8(..)
+, D9(..)
+, DA(..)
+, DB(..)
+, DC(..)
+, DD(..)
+, DE(..)
+, DF(..)
+) where
+
+import Control.Lens
+
+type DecimalNoZero d =
+  (D1 d, D2 d, D3 d, D4 d, D5 d, D6 d, D7 d, D8 d, D9 d)
+
+type Decimal d =
+  (D0 d, DecimalNoZero d)
+
+type OctalNoZero d =
+  (D1 d, D2 d, D3 d, D4 d, D5 d, D6 d, D7 d)
+
+type Octal d =
+  (D0 d, OctalNoZero d)
+
+type HexadecimalNoZero d =
+  (D1 d, D2 d, D3 d, D4 d, D5 d, D6 d, D7 d, D8 d, D9 d, DA d, DB d, DC d, DD d, DE d, DF d)
+
+type Hexadecimal d =
+  (D0 d, HexadecimalNoZero d)
+
+class D0 d where
+  d0 ::
+    Prism'
+      d
+      ()
+  x0 ::
+    d
+  x0 =
+    d0 # ()
+
+instance D0 () where
+  d0 =
+    id
+
+class D1 d where
+  d1 ::
+    Prism'
+      d
+      ()
+  x1 ::
+    d
+  x1 =
+    d1 # ()
+
+instance D1 () where
+  d1 =
+    id
+    
+class D2 d where
+  d2 ::
+    Prism'
+      d
+      ()
+  x2 ::
+    d
+  x2 =
+    d2 # ()
+
+instance D2 () where
+  d2 =
+    id
+    
+class D3 d where
+  d3 ::
+    Prism'
+      d
+      ()
+  x3 ::
+    d
+  x3 =
+    d3 # ()
+
+instance D3 () where
+  d3 =
+    id
+    
+class D4 d where
+  d4 ::
+    Prism'
+      d
+      ()
+  x4 ::
+    d
+  x4 =
+    d4 # ()
+
+instance D4 () where
+  d4 =
+    id
+    
+class D5 d where
+  d5 ::
+    Prism'
+      d
+      ()
+  x5 ::
+    d
+  x5 =
+    d5 # ()
+
+instance D5 () where
+  d5 =
+    id
+    
+class D6 d where
+  d6 ::
+    Prism'
+      d
+      ()
+  x6 ::
+    d
+  x6 =
+    d6 # ()
+
+instance D6 () where
+  d6 =
+    id
+    
+class D7 d where
+  d7 ::
+    Prism'
+      d
+      ()
+  x7 ::
+    d
+  x7 =
+    d7 # ()
+
+instance D7 () where
+  d7 =
+    id
+    
+class D8 d where
+  d8 ::
+    Prism'
+      d
+      ()
+  x8 ::
+    d
+  x8 =
+    d8 # ()
+
+instance D8 () where
+  d8 =
+    id
+    
+class D9 d where
+  d9 ::
+    Prism'
+      d
+      ()
+  x9 ::
+    d
+  x9 =
+    d9 # ()
+
+instance D9 () where
+  d9 =
+    id
+    
+class DA d where
+  dA ::
+    Prism'
+      d
+      ()
+  xA ::
+    d
+  xA =
+    dA # ()
+
+instance DA () where
+  dA =
+    id
+    
+class DB d where
+  dB ::
+    Prism'
+      d
+      ()
+  xB ::
+    d
+  xB =
+    dB # ()
+
+instance DB () where
+  dB =
+    id
+    
+class DC d where
+  dC ::
+    Prism'
+      d
+      ()
+  xC ::
+    d
+  xC =
+    dC # ()
+
+instance DC () where
+  dC =
+    id
+    
+class DD d where
+  dD ::
+    Prism'
+      d
+      ()
+  xD ::
+    d
+  xD =
+    dD # ()
+
+instance DD () where
+  dD =
+    id
+    
+class DE d where
+  dE ::
+    Prism'
+      d
+      ()
+  xE ::
+    d
+  xE =
+    dE # ()
+
+instance DE () where
+  dE =
+    id
+    
+class DF d where
+  dF ::
+    Prism'
+      d
+      ()
+  xF ::
+    d
+  xF =
+    dF # ()
+
+instance DF () where
+  dF =
+    id
+    
