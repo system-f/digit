c66b840702d201f2d98d90ce67d8aedd43af2d02
Tony Morris <tmorris@tmorris.net>
2017-08-18 18:09:48 +1000

    prisms for Char and Integral

--- src/Data/Digit.hs
+++ src/Data/Digit.hs
@@ -60,3 +60,202 @@ import Data.Digit.Digite as D
 import Data.Digit.DigitE as D
 import Data.Digit.Digitf as D
 import Data.Digit.DigitF as D
+
+import Control.Lens
+import Control.Lens.Extras(is)
+import Data.List
+import Data.Maybe
+import Prelude
+
+-- Integral:
+--   BinaryNotZero, Binary, Octal, OctalNotZero, Decimal, DecimalNotZero, Hexadecimal, HexadecimalNotZero, HeXaDeCiMaL, HeXaDeCiMaLNotZero, HEXADECIMAL, HEXADECIMALNotZero
+-- Char:
+--   BinaryNotZero, Binary, Octal, OctalNotZero, Decimal, DecimalNotZero, Hexadecimal, HexadecimalNotZero, HeXaDeCiMaL, HeXaDeCiMaLNotZero, HEXADECIMAL, HEXADECIMALNotZero
+
+associatePrism ::
+  (Eq b, Choice p, Applicative f) =>
+  (b, APrism a a () ())
+  -> [(b, APrism a a () ())]
+  -> p a (f a)
+  -> p b (f b)
+associatePrism def z =
+  prism'
+    (\d -> fst (fromMaybe def (find (\(_, w) -> is w d) z)))
+    (\i -> (\p -> clonePrism p # ()) <$> lookup i (def:z))
+
+integralBinaryNoZero ::
+  (Integral a, BinaryNoZero d) =>
+  Prism'
+    a
+    d
+integralBinaryNoZero =
+  associatePrism (1, d1) []
+  
+integralBinary ::
+  (Integral a, Binary d) =>
+  Prism'
+    a
+    d
+integralBinary =
+  associatePrism (0, d0) [(1, d1)]
+  
+integralOctalNoZero ::
+  (Integral a, OctalNoZero d) =>
+  Prism'
+    a
+    d
+integralOctalNoZero =
+  associatePrism (1, d1) [(2, d2), (3, d3), (4, d4), (5, d5), (6, d6), (7, d7)]
+  
+integralOctal ::
+  (Integral a, Octal d) =>
+  Prism'
+    a
+    d
+integralOctal =
+  associatePrism (0, d0) [(1, d1), (2, d2), (3, d3), (4, d4), (5, d5), (6, d6), (7, d7)]
+  
+integralDecimalNoZero ::
+  (Integral a, DecimalNoZero d) =>
+  Prism'
+    a
+    d
+integralDecimalNoZero =
+  associatePrism (1, d1) [(2, d2), (3, d3), (4, d4), (5, d5), (6, d6), (7, d7), (8, d8), (9, d9)]
+  
+integralDecimal ::
+  (Integral a, Decimal d) =>
+  Prism'
+    a
+    d
+integralDecimal =
+  associatePrism (0, d0) [(1, d1), (2, d2), (3, d3), (4, d4), (5, d5), (6, d6), (7, d7), (8, d8), (9, d9)]
+  
+integralHexadecimalNoZero ::
+  (Integral a, HexadecimalNoZero d) =>
+  Prism'
+    a
+    d
+integralHexadecimalNoZero =
+  associatePrism (1, d1) [(2, d2), (3, d3), (4, d4), (5, d5), (6, d6), (7, d7), (8, d8), (9, d9), (10, da), (11, db), (12, dc), (13, dd), (14, de), (15, df)]
+  
+integralHexadecimal ::
+  (Integral a, Hexadecimal d) =>
+  Prism'
+    a
+    d
+integralHexadecimal =
+  associatePrism (0, d0) [(1, d1), (2, d2), (3, d3), (4, d4), (5, d5), (6, d6), (7, d7), (8, d8), (9, d9), (10, da), (11, db), (12, dc), (13, dd), (14, de), (15, df)]
+  
+integralHEXADECIMALNoZero ::
+  (Integral a, HEXADECIMALNoZero d) =>
+  Prism'
+    a
+    d
+integralHEXADECIMALNoZero =
+  associatePrism (1, d1) [(2, d2), (3, d3), (4, d4), (5, d5), (6, d6), (7, d7), (8, d8), (9, d9), (10, dA), (11, dB), (12, dC), (13, dD), (14, dE), (15, dF)]
+  
+integralHEXADECIMAL ::
+  (Integral a, HEXADECIMAL d) =>
+  Prism'
+    a
+    d
+integralHEXADECIMAL =
+  associatePrism (0, d0) [(1, d1), (2, d2), (3, d3), (4, d4), (5, d5), (6, d6), (7, d7), (8, d8), (9, d9), (10, dA), (11, dB), (12, dC), (13, dD), (14, dE), (15, dF)]
+  
+charBinaryNoZero ::
+  BinaryNoZero d =>
+  Prism'
+    Char
+    d
+charBinaryNoZero =
+  associatePrism ('1', d1) []
+  
+charBinary ::
+  Binary d =>
+  Prism'
+    Char
+    d
+charBinary =
+  associatePrism ('0', d0) [('1', d1)]
+  
+charOctalNoZero ::
+  OctalNoZero d =>
+  Prism'
+    Char
+    d
+charOctalNoZero =
+  associatePrism ('1', d1) [('2', d2), ('3', d3), ('4', d4), ('5', d5), ('6', d6), ('7', d7)]
+  
+charOctal ::
+  Octal d =>
+  Prism'
+    Char
+    d
+charOctal =
+  associatePrism ('0', d0) [('1', d1), ('2', d2), ('3', d3), ('4', d4), ('5', d5), ('6', d6), ('7', d7)]
+  
+charDecimalNoZero ::
+  DecimalNoZero d =>
+  Prism'
+    Char
+    d
+charDecimalNoZero =
+  associatePrism ('1', d1) [('2', d2), ('3', d3), ('4', d4), ('5', d5), ('6', d6), ('7', d7), ('8', d8), ('9', d9)]
+  
+charDecimal ::
+  Decimal d =>
+  Prism'
+    Char
+    d
+charDecimal =
+  associatePrism ('0', d0) [('1', d1), ('2', d2), ('3', d3), ('4', d4), ('5', d5), ('6', d6), ('7', d7), ('8', d8), ('9', d9)]
+  
+charHexadecimalNoZero ::
+  HexadecimalNoZero d =>
+  Prism'
+    Char
+    d
+charHexadecimalNoZero =
+  associatePrism ('1', d1) [('2', d2), ('3', d3), ('4', d4), ('5', d5), ('6', d6), ('7', d7), ('8', d8), ('9', d9), ('a', da), ('b', db), ('c', dc), ('d', dd), ('e', de), ('f', df)]
+  
+charHexadecimal ::
+  Hexadecimal d =>
+  Prism'
+    Char
+    d
+charHexadecimal =
+  associatePrism ('0', d0) [('1', d1), ('2', d2), ('3', d3), ('4', d4), ('5', d5), ('6', d6), ('7', d7), ('8', d8), ('9', d9), ('a', da), ('b', db), ('c', dc), ('d', dd), ('e', de), ('f', df)]
+  
+charHEXADECIMALNoZero ::
+  HEXADECIMALNoZero d =>
+  Prism'
+    Char
+    d
+charHEXADECIMALNoZero =
+  associatePrism ('1', d1) [('2', d2), ('3', d3), ('4', d4), ('5', d5), ('6', d6), ('7', d7), ('8', d8), ('9', d9), ('A', dA), ('B', dB), ('C', dC), ('D', dD), ('E', dE), ('F', dF)]
+  
+charHEXADECIMAL ::
+  HEXADECIMAL d =>
+  Prism'
+    Char
+    d
+charHEXADECIMAL =
+  associatePrism ('0', d0) [('1', d1), ('2', d2), ('3', d3), ('4', d4), ('5', d5), ('6', d6), ('7', d7), ('8', d8), ('9', d9), ('A', dA), ('B', dB), ('C', dC), ('D', dD), ('E', dE), ('F', dF)]
+  
+charHeXaDeCiMaLNoZero ::
+  HeXaDeCiMaLNoZero d =>
+  Prism'
+    Char
+    d
+charHeXaDeCiMaLNoZero =
+  associatePrism ('1', d1) [('2', d2), ('3', d3), ('4', d4), ('5', d5), ('6', d6), ('7', d7), ('8', d8), ('9', d9), ('a', da), ('b', db), ('c', dc), ('d', dd), ('e', de), ('f', df), ('A', dA), ('B', dB), ('C', dC), ('D', dD), ('E', dE), ('F', dF)]
+  
+charHeXaDeCiMaL ::
+  HeXaDeCiMaL d =>
+  Prism'
+    Char
+    d
+charHeXaDeCiMaL =
+  associatePrism ('0', d0) [('1', d1), ('2', d2), ('3', d3), ('4', d4), ('5', d5), ('6', d6), ('7', d7), ('8', d8), ('9', d9), ('a', da), ('b', db), ('c', dc), ('d', dd), ('e', de), ('f', df), ('A', dA), ('B', dB), ('C', dC), ('D', dD), ('E', dE), ('F', dF)]
+  
