0736103c86defc1cda1e9762f9fe05e8d2b6d783
Tony Morris <tmorris@tmorris.net>
2017-09-06 17:43:33 +1000

    Char prisms

--- digit.nix
+++ digit.nix
@@ -1,7 +1,6 @@
-{ mkDerivation, ansi-wl-pprint, base, directory, filepath, hedgehog
-, lens, papa, parsers, pretty, process, semigroupoids, semigroups
-, stdenv, tasty, tasty-hedgehog, tasty-hspec, template-haskell
-, text, transformers
+{ mkDerivation, ansi-wl-pprint, base, hedgehog, lens, papa, parsers
+, pretty, semigroupoids, semigroups, stdenv, tasty, tasty-hedgehog
+, tasty-hspec, tasty-hunit, template-haskell, text
 }:
 mkDerivation {
   pname = "digit";
@@ -11,8 +10,8 @@ mkDerivation {
     base lens papa parsers semigroupoids semigroups template-haskell
   ];
   testHaskellDepends = [
-    ansi-wl-pprint base directory filepath hedgehog papa pretty process
-    tasty tasty-hedgehog tasty-hspec text transformers
+    ansi-wl-pprint base hedgehog lens papa parsers pretty tasty
+    tasty-hedgehog tasty-hspec tasty-hunit text
   ];
   homepage = "https://github.com/qfpl/digit";
   description = "A data-type representing digits 0-9 and other combinations";
--- test/Main.hs
+++ test/Main.hs
@@ -1,17 +1,55 @@
-module Main where
+{-# LANGUAGE RankNTypes #-}
 
+module Main(
+  main
+) where
+
+import Data.Digit
 import Papa
 import Hedgehog
 import Test.Tasty
+import Test.Tasty.Hedgehog
+import Test.Tasty.HUnit
 import qualified Hedgehog.Gen as Gen
-import qualified Hedgehog.Range as Range
 
-main :: IO ()
-main = defaultMain $
-  testGroup "tasty-hedgehog tests" []
+charPrism ::
+  String
+  -> Prism' Char Digit
+  -> [(Char, Digit)]
+  -> [TestTree]
+charPrism n p x =
+  testProperty
+    (n ++ " prism invalid values")
+    (
+      property $
+        do  c <- forAll . Gen.filter (`notElem` (map fst x)) . Gen.choice $ [Gen.hexit, Gen.unicode]
+            (c ^? p :: Maybe Digit) === Nothing
+    ) :
+    (
+      x >>= \(c, d) ->
+        [
+          testCase
+            (n ++ " prism ->")
+            ((c ^? p :: Maybe Digit) @?= Just d)
+        , testCase
+            (n ++ " prism <-")
+            (p # d @?= c)
+        ]
+    )
 
-prop_reverse :: Property
-prop_reverse =
-  property $ do
-    xs <- forAll $ Gen.list (Range.linear 0 100) Gen.alpha
-    reverse (reverse xs) === xs
+main ::
+  IO ()
+main =
+  defaultMain $
+    testGroup "digit tests" $
+      concat [
+        charPrism "charBinaryNoZero" charBinaryNoZero [('1', Digit1)]
+      , charPrism "charBinary" charBinary [('0', Digit0), ('1', Digit1)]
+      , charPrism "charOctalNoZero" charOctalNoZero [('1', Digit1), ('2', Digit2), ('3', Digit3), ('4', Digit4), ('5', Digit5), ('6', Digit6), ('7', Digit7)]
+      , charPrism "charOctal" charOctal [('0', Digit0), ('1', Digit1), ('2', Digit2), ('3', Digit3), ('4', Digit4), ('5', Digit5), ('6', Digit6), ('7', Digit7)]
+      , charPrism "charDecimalNoZero" charDecimalNoZero [('1', Digit1), ('2', Digit2), ('3', Digit3), ('4', Digit4), ('5', Digit5), ('6', Digit6), ('7', Digit7), ('8', Digit8), ('9', Digit9)]
+      , charPrism "charDecimal" charDecimal [('0', Digit0), ('1', Digit1), ('2', Digit2), ('3', Digit3), ('4', Digit4), ('5', Digit5), ('6', Digit6), ('7', Digit7), ('8', Digit8), ('9', Digit9)]
+      , charPrism "charHexadecimalNoZero" charHexadecimalNoZero [('1', Digit1), ('2', Digit2), ('3', Digit3), ('4', Digit4), ('5', Digit5), ('6', Digit6), ('7', Digit7), ('8', Digit8), ('9', Digit9), ('a', Digita), ('b', Digitb), ('c', Digitc), ('d', Digitd), ('e', Digite), ('f', Digitf)]
+      , charPrism "charHEXADECIMALNoZero" charHEXADECIMALNoZero [('1', Digit1), ('2', Digit2), ('3', Digit3), ('4', Digit4), ('5', Digit5), ('6', Digit6), ('7', Digit7), ('8', Digit8), ('9', Digit9), ('A', DigitA), ('B', DigitB), ('C', DigitC), ('D', DigitD), ('E', DigitE), ('F', DigitF)]
+      , charPrism "charHeXaDeCiMaLNoZero" charHeXaDeCiMaLNoZero [('1', Digit1), ('2', Digit2), ('3', Digit3), ('4', Digit4), ('5', Digit5), ('6', Digit6), ('7', Digit7), ('8', Digit8), ('9', Digit9), ('a', Digita), ('b', Digitb), ('c', Digitc), ('d', Digitd), ('e', Digite), ('f', Digitf), ('A', DigitA), ('B', DigitB), ('C', DigitC), ('D', DigitD), ('E', DigitE), ('F', DigitF)]
+      ]
